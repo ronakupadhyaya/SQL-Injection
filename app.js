00590342e11cde334dc591062e04bffc370c1e8d
pradyutbansal <pradyutbansal@gmail.com>
2017-06-22 08:56:16 -0700

    completed still this part

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var models = require('./models');
-
+var cookieSession = require('cookie-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
@@ -22,6 +22,11 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['masafu']
+}))
+
 app.use(cookieParser());
 app.use(morgan('combined'));
 
@@ -30,11 +35,27 @@ app.get('/', function(req, res) {
     stage2: getSecret('stage2')
   });
 });
+//Stage 1 client side
+app.post("/", function(req,res){
+  if(req.body.password === "gingerbread"){
+    res.redirect("/stage2");
+  }else{
+    res.redirect("/");
+  };
+});
+//stage 2 Insecure cookies
+// app.use('/', function(req,res,next){
+//   if(req.session.secureCookie){
+//     next();
+//   }else{
+//     res.redirect('/');
+//   }
+// })
 
 app.get('/' + getSecret('stage2'), function(req, res) {
   res.render('stage2', {
-    user: req.cookies.user,
-    admin: req.cookies.user === 'admin',
+    user: req.session.secureCookie,
+    admin: req.session.secureCookie === 'admin',
     stage3: getSecret('stage3')
   });
 });
@@ -42,7 +63,7 @@ app.get('/' + getSecret('stage2'), function(req, res) {
 app.post('/' + getSecret('stage2'), function(req, res) {
   console.log(req.body);
   if (req.body.username === 'bob' && req.body.password === 'baseball') {
-    res.cookie('user', 'bob');
+    req.session.secureCookie = req.body.username;
     res.redirect('/' + getSecret('stage2'));
   } else {
     res.sendStatus(401);
@@ -57,10 +78,11 @@ app.get('/' + getSecret('stage3'), function(req, res) {
 
 app.post('/' + getSecret('stage3'), function(req, res) {
   var secret = req.body.secret;
+  console.log(typeof secret);
   models.Secret.findOne({
     secret: secret
   }, function(error, secret) {
-    if (error) {
+    if (error || typeof secret !== 'string') {
       res.status(400).json({
         error: error
       });
@@ -76,6 +98,7 @@ app.post('/' + getSecret('stage3'), function(req, res) {
     }
   });
 });
+
 
 app.get('/' + getSecret('stage4'), function(req, res) {
   res.render('stage4');
---
+++ env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI=mongodb://bansal:bansal@ds135252.mlab.com:35252/hacksack
