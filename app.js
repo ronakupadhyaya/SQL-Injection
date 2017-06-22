dfeb0da02ba103b21b9a65546175a8ff96db0e1a
Jeffrey Chen <jeffche@seas.upenn.edu>
2017-06-22 09:33:25 -0700

    Add steal-this-app

--- app.js
+++ app.js
@@ -25,24 +25,66 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan('combined'));
 
+// app.get('/setCookie', function(req, res){
+//   req.session.secureCookie = req.cookie;
+//   res.send('Done!')
+// });
+//
+// app.get('/checkCookie', function(req, res){
+//   if (req.session.secureCookie === req.cookie) {
+//     res.send('Good!');
+//   } else {
+//     res.status(400).send('Bad!');
+//   }
+// });
+
+var cookieSession = require('cookie-session');
+app.use(cookieSession({
+  name: 'session',
+  keys: ['secret key here'],
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}));
+
 app.get('/', function(req, res) {
-  res.render('stage1', {
-    stage2: getSecret('stage2')
-  });
+  if(req.cookies.stageOnePass){
+    res.redirect('/stage2');
+  }
+  else{
+    res.render('stage1', {
+      stage2: getSecret('stage2')
+    });
+  }
 });
 
 app.get('/' + getSecret('stage2'), function(req, res) {
+  // MOOSE'S STUFF
+  req.session.visited = 1 + parseInt(req.session.visited || 0);
+  console.log(req.session.visited);
+  // END MOOSE'S STUFF
   res.render('stage2', {
-    user: req.cookies.user,
-    admin: req.cookies.user === 'admin',
+    // USED TO BE req.cookie.user
+    user: req.session.user,
+    admin: req.session.user === 'admin',
     stage3: getSecret('stage3')
   });
 });
 
+app.post('/', function(req, res){
+  if(req.body.password === 'gingerbread'){
+    res.cookie('stageOnePass', 'passed');
+    res.redirect('/stage2');
+  }
+  else{
+    res.redirect('/');
+  }
+})
+
 app.post('/' + getSecret('stage2'), function(req, res) {
   console.log(req.body);
   if (req.body.username === 'bob' && req.body.password === 'baseball') {
-    res.cookie('user', 'bob');
+    // res.cookie('user', 'bob');
+    req.session.user = 'admin';
     res.redirect('/' + getSecret('stage2'));
   } else {
     res.sendStatus(401);
@@ -57,6 +99,11 @@ app.get('/' + getSecret('stage3'), function(req, res) {
 
 app.post('/' + getSecret('stage3'), function(req, res) {
   var secret = req.body.secret;
+  if(typeof secret !== "string"){
+    res.status(400).json({
+      error: "Secret must be string!"
+    });
+  }
   models.Secret.findOne({
     secret: secret
   }, function(error, secret) {
@@ -80,6 +127,16 @@ app.post('/' + getSecret('stage3'), function(req, res) {
 app.get('/' + getSecret('stage4'), function(req, res) {
   res.render('stage4');
 });
+
+
+app.use(function(req, res, next){
+  if(req.cookies.stageOnePass){
+    next();
+  }
+  else{
+    res.redirect('/');
+  }
+})
 
 app.use('/exercise2', require('./exercise2'));
 
---
+++ env.sh
@@ -0,0 +1,1 @@
+export MONGODB_URI='mongodb://jeffreyxchen:1234@ds133582.mlab.com:33582/hacker-week4day3'
