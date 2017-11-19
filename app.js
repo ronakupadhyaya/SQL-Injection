bf79e4adda46a97505fa6c60a41980a7d68a671a
crawforde <crawforde@kenyon.edu>
2017-11-18 16:46:26 -0800

    Finished

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var path = require('path');
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var models = require('./models');
-
+var session = require('cookie-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
@@ -15,7 +15,6 @@ function getSecret(key) {
   return process.env[key] || key;
 }
 
-
 app.engine('hbs', exphbs({extname: 'hbs', defaultLayout: 'main'}));
 app.set('view engine', 'hbs');
 
@@ -24,6 +23,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan('combined'));
+app.use(session({name: 'session', keys: ['key1']}));
+var csrf = require('csurf')();
+app.use(csrf);
+
+
+app.use('/route', function(req, res) {
+  res.render('template', {
+    _csrf: req.csrfToken()
+  });
+});
+
+app.get('/setCookie', function(req, res){
+  req.session.secureCookie = 'cookie value';
+  res.send('Done!')
+});
+
+app.get('/checkCookie', function(req, res){
+  if (req.session.secureCookie === 'cookie value') {
+    res.send('Good!');
+  } else {
+    res.status(400).send('Bad!');
+  }
+});
 
 app.get('/', function(req, res) {
   res.render('stage1', {
@@ -31,10 +53,19 @@ app.get('/', function(req, res) {
   });
 });
 
+app.post('/',function(req,res){                       //Edited
+  if(getSecret(req.body.password)==='gingerbread'){
+    res.redirect('/stage2');
+  }
+  else{
+    res.redirect('/');
+  }
+});
+
 app.get('/' + getSecret('stage2'), function(req, res) {
   res.render('stage2', {
-    user: req.cookies.user,
-    admin: req.cookies.user === 'admin',
+    user: req.session.user,
+    admin: req.session.user === 'admin',
     stage3: getSecret('stage3')
   });
 });
@@ -42,7 +73,7 @@ app.get('/' + getSecret('stage2'), function(req, res) {
 app.post('/' + getSecret('stage2'), function(req, res) {
   console.log(req.body);
   if (req.body.username === 'bob' && req.body.password === 'baseball') {
-    res.cookie('user', 'bob');
+    req.session.user='bob';
     res.redirect('/' + getSecret('stage2'));
   } else {
     res.sendStatus(401);
@@ -57,6 +88,11 @@ app.get('/' + getSecret('stage3'), function(req, res) {
 
 app.post('/' + getSecret('stage3'), function(req, res) {
   var secret = req.body.secret;
+  if(typeof secret !== 'String'){
+    res.status(401).json({
+      error: "Hack"
+    });
+  }
   models.Secret.findOne({
     secret: secret
   }, function(error, secret) {
